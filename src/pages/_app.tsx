8d783c2368f41c9fe5b554335544fa27ee545b3c
Davood Valaie <44891047+dabi-vm@users.noreply.github.com>
2024-01-22 22:15:46 +0330

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import "@farakav-challenge/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
