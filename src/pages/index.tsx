5fb4d09dfd835c7d06e52719e350189ff03e42ed
Davood Valaie <44891047+dabi-vm@users.noreply.github.com>
2024-01-23 15:21:33 +0330

    feat: add providers wrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import Layout from "@farakav-challenge/components/layout";
+import Providers from "@farakav-challenge/components/providers";
 import { PropsWithChildren } from "react";
 
 export default function Home({ children }: PropsWithChildren<{}>) {
-  return <Layout>{children}</Layout>;
+  return (
+    <Providers>
+      <Layout>{children}</Layout>
+    </Providers>
+  );
 }
